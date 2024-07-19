cd6618d279e2e812ca6e3ebad8b3e233e54cc34c
amirhossein-github <amirhossein.business.developer@gmail.com>
2024-07-20 01:01:09 +0330

    test

---
+++ main.js
@@ -0,0 +1,63 @@
+let localSchool = {
+    classA:{
+        StA1:{        
+            firstName:'lulu',
+            lastName:'meow',
+            phoneNumber:'none of your Shit:)',
+            score:'19',
+            age:'18',
+            gender:'female',
+            id:'lulu with a B'
+        },
+        StA2:{        
+            firstName:'yuji',
+            lastName:'itadori',
+            phoneNumber:'01155284600',
+            score:'18',
+            age:'14',
+            gender:'male',
+            id:'CuteAF MF'
+        },
+        StA3:{        
+            firstName:'johnny',
+            lastName:'soons',
+            phoneNumber:'911',
+            score:'21',
+            age:'45',
+            gender:'male',
+            id:'Smashin it'
+        }
+
+    },
+    classB:{ 
+        StB1:{        
+            firstName:'ahmed',
+            lastName:'ghasemi',
+            phoneNumber:'09405206911',
+            score:'0.25',
+            age:'29',
+            gender:'male',
+            id:'ahmedLX'
+        },
+        StB2:{        
+            firstName:'damon',
+            lastName:'salvatore',
+            phoneNumber:'00116985',
+            score:'11',
+            age:'530',
+            gender:'male',
+            id:'Charming Vamp'
+        },
+        StB3:{        
+            firstName:'lily',
+            lastName:'loop',
+            phoneNumber:'',
+            score:'19.75',
+            age:'17',
+            gender:'female',
+            id:'nerdAF'
+        }
+    }
+
+}
+console.log(localSchool);
